d50f2550f850a36db4863ed0d5e28778352ee8bd
Takashi Mori <kanetugu2015@gmail.com>
2022-10-04 08:15:16 +0900

    add reset

---
+++ bin/mm-reset.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source env/env.bash
+
+bash bin/mm-status.bash  | grep Active | grep running
+if [ $? -eq 0 ]
+then
+    echo "ERROR: mattermost is running. please stop this service"
+    exit 1
+fi
+
+echo "INFO: Initializing DB"
+(cd ${DB_TOOL_PATH} && bash bin/db_drop.bash ${MATTERMOST_DBNAME})
+(cd ${DB_TOOL_PATH} && bash bin/db_create.bash ${MATTERMOST_DBNAME})
+
+echo "INFO: Creating DB..."
+bash bin/mm-start.bash
+bash bin/mm-stop.bash
+echo "INFO: DONE"
--- env/env.bash
+++ env/env.bash
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+export DB_TOOL_PATH=`pwd`/db-backup-restore
 export MATTERMOST_CMD_PATH=/opt/mattermost/bin
 export MATTERMOST_CMD=${MATTERMOST_CMD_PATH}/mmctl
+export MATTERMOST_DBNAME=mattermost
+
+source ${DB_TOOL_PATH}/env/env.bash
 
 export PRIVATE_OPTION=
 function get_private_option()
